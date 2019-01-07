0870195daa5bf52c855d46615b77e70b4583719a
Simon grahn <simon.grahn@gmail.com>
2019-01-07 12:36:25 +0100

    last push before moving on to next design

--- home.js
+++ home.js
@@ -1,14 +1,22 @@
 // const url;
 // const apiKey;
 const artist = $('#artist');
-const song = $('song');
+const song = $('#song');
 const addSong = $('#add-song');
 
 $(document).ready(() => {
 
+
+
+
+
   addSong.on('click', event => {
 
-    
+
+
+
+
+
   });
 
 
