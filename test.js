fae424ed1d9865c1dbf3a81fa79f59a04c2d0776
Simon grahn <simon.grahn@gmail.com>
2019-01-07 16:04:02 +0100

    css

---
+++ test.js
@@ -0,0 +1,9 @@
+
+/*TESTTESTTESTTESTTESTTEST*/
+
+$(".lineInTheList").append("<li>" + artist + " - " + title + "</li>");// +
+ '<button id="getLyrics">Get the lyrics</button>');
+
+
+
+$(".lineInTheList").append(`<li> {artist} - {title} <button class="buttonJs">get lyrics</button></li>`);
