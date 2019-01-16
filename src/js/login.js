3a425d14347488175073edf1ca9e84dba5988f37
Simon grahn <simon.grahn@gmail.com>
2019-01-16 11:11:19 +0100

    change size of pic

--- src/js/login.js
+++ src/js/login.js
@@ -1,30 +1,25 @@
+$("input").focus(function() {
+  $(this).prev(".placeholder").addClass("active");
 
-  $("input").focus(function() {
-    $(this).prev(".placeholder").addClass("active");
+})
 
-  })
-  
-  $(".send").focus(function() {
-    $("input[type=submit]").addClass("active");
+$(".send").focus(function() {
+  $("input[type=submit]").addClass("active");
 
-  })
-  
-  $("#submit").click(function() {
-    $("#submit").addClass("active");
-    $("#wrapper").addClass("active");
-  })
-  
-  
-  function check(form)
-{
- 
- if(form.userid.value == "admin@hotmail.se" && form.pswrd.value == "admin")
-  {
+})
+
+$("#submit").click(function() {
+  $("#submit").addClass("active");
+  $("#wrapper").addClass("active");
+})
+
+
+function check(form) {
+
+  if (form.userid.value == "admin@hotmail.se" && form.pswrd.value == "admin") {
     location.replace("app.html");
 
-  }
- else
- {
-   alert("Your username or password is incorrect.")
+  } else {
+    alert("Your username or password is incorrect.")
   }
 }
